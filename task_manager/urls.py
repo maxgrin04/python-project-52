d886472c53ef4d49710bc472640c749a2448c52e
maxgrin04 <mgrin98@gmail.com>
2025-09-16 23:19:35 +0900

    Result

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from .views import CustomLoginView, CustomLogoutView, index
+from .views import CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('users/', include('task_manager.users.urls')),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
--- task_manager/views.py
+++ task_manager/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-from django.http import HttpResponse
 
 
 class IndexView(TemplateView):
@@ -22,9 +21,3 @@ class CustomLogoutView(SuccessMessageMixin, LogoutView):
     def dispatch(self, request, *args, **kwargs):
         messages.info(request, "Вы разлогинены")
         return super().dispatch(request, *args, **kwargs)
-
-
-def index(request):
-    a = None
-    a.hello() # Creating an error with an invalid line of code
-    return HttpResponse("Hello, world. You're at the pollapp index.")
