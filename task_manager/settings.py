08408ac1e36fd979247a411e26150ae06009bbb9
maxgrin04 <mgrin98@gmail.com>
2025-09-18 21:12:31 +0900

    Result

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-p(tan!(wb0(4^5ad@)*der175uh3d3&1itkp&vm%jqhsct@bqy')
+SECRET_KEY = os.getenv('SECRET_KEY', 'gh(9^^f3!@9r1naa5=f22-u@rvk087-byf156gihao=$%p=bi%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False')
